49eb162e587cce1c0b1bf1a8a24e987be9c38cf3
sudhanshu201 <113603064+sudhanshu201@users.noreply.github.com>
2023-01-22 20:06:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,30 @@
-function letterCombinations(input_digit) {
-  //Complete the function
+function letterCombinations(digits) {
+    //create an array of strings representing the letters associated with each digit
+    let mapping = ["0", "1", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"];
+    let result = [];
+
+    //edge case: if the input string is empty, return an empty array
+    if (!digits) {
+        return result;
+    }
+
+    //recursive function to generate all possible letter combinations
+    function generateCombinations(combination, nextDigits) {
+        if (nextDigits.length === 0) {
+            result.push(combination);
+        } else {
+            let digit = nextDigits.substring(0, 1);
+            let letters = mapping[digit];
+            for (let i = 0; i < letters.length; i++) {
+                let letter = letters.substring(i, i+1);
+                generateCombinations(combination + letter, nextDigits.substring(1));
+            }
+        }
+    }
+
+    generateCombinations("", digits);
+    return result.sort();
 }
+
 
 module.exports = letterCombinations;
